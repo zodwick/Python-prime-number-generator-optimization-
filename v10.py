50f86a9964bd06300613f8c4a14b74db3509fff9
zodwick <115868723+zodwick@users.noreply.github.com>
2022-12-10 21:36:52 +0530

    day 02

---
+++ v10.py
@@ -0,0 +1,55 @@
+# added break
+# filter out even
+# filter out even in for loop directly
+# limit search to half of i
+# limit search to square root of i
+# limit search to only previous prime numbers
+
+# limit search to only previous prime numbers but checking for less than directly at list addition rather than during modulus
+# check
+
+# taking finding of sqrt of final number as constant outside
+
+import time
+start = time.time()
+n=100000
+p = int(n**0.5)
+
+c = 0
+k = 1
+l = []
+
+for i in range(3, n, 2):
+
+    for j in l:
+       
+        if i % j == 0:
+            c += 1
+            break
+    if c == 0:
+        k += 1
+        if i < p:
+            l.append(i)
+    c = 0
+
+print(k)
+
+end = time.time()
+print(end - start)
+
+
+# time took
+# 258.44545817375183
+# 24.8780837059021
+# 23.12345814704895
+# 22.465441465377808
+# 11.63539743423462
+# 0.11180853843688965
+# 0.09911322593688965
+# 0.03931570053100586
+# 0.03710675239562988
+
+
+
+
+#16.366311073303223  for 10000000 values
---
+++ v11.py
@@ -0,0 +1,44 @@
+# added break
+# filter out even
+# filter out even in for loop directly
+# limit search to half of i
+# limit search to square root of i
+# limit search to only previous prime numbers
+
+# limit search to only previous prime numbers but checking for less than directly at list addition rather than during modulus
+# check
+
+# taking finding of sqrt of final number as constant outside
+
+# implementing sieve method?
+
+import time
+start = time.time()
+n = 100000
+numbers = set(range(2, n+1))
+primes = []
+
+while numbers:
+    p = numbers.pop()
+    primes.append(p)
+    numbers.difference_update(set(range(2*p, n+1, p)))
+    # 2*p as p(the first element of numbers) was already removed as part of pop() function
+
+
+print(len(primes))
+
+end = time.time()
+print(end - start)
+
+
+# time took (seconds)
+# 258.44545817375183
+# 24.8780837059021
+# 23.12345814704895
+# 22.465441465377808
+# 11.63539743423462
+# 0.11180853843688965
+# 0.09911322593688965
+# 0.03931570053100586
+# 0.03710675239562988
+# 0.020193815231323242
